73fe28236ea46c7dd024a71ded16aa511863fc73
Charlie Sharpsteen <source@sharpsteen.net>
2017-08-27 14:52:13 -0700

    Use service context to store mount data
    This patch re-factors the FileServing service to store the state of
    environments and mount paths read from JRuby as part of the service
    context.

--- src/clj/puppetlabs/services/file_serving/file_serving_core.clj
+++ src/clj/puppetlabs/services/file_serving/file_serving_core.clj
@@ -1,4 +1,6 @@
 (ns puppetlabs.services.file-serving.file-serving-core
+  (:require
+   [puppetlabs.bodgery.jruby :as jruby])
   (:import
    (org.eclipse.jetty.servlet ServletContextHandler ServletHolder DefaultServlet)
    (org.eclipse.jetty.util.resource ResourceCollection)))
@@ -21,3 +23,13 @@
   "Say hello to caller"
   [caller]
   (format "Hello, %s!" caller))
+
+(defn refresh-jruby-state!
+  "Refreshes the state of environments and mount points visible to the Ruby
+  layer."
+  [context]
+  (let [jruby-service (:jruby-service context)]
+    (reset! (:environments context)
+            (jruby/run-script! jruby-service "file_serving_shims/get_environments.rb"))
+    (reset! (:ruby-mounts context)
+            (jruby/run-script! jruby-service "file_serving_shims/get_mounts.rb"))))
--- src/clj/puppetlabs/services/file_serving/file_serving_service.clj
+++ src/clj/puppetlabs/services/file_serving/file_serving_service.clj
@@ -1,7 +1,6 @@
 (ns puppetlabs.services.file-serving.file-serving-service
   (:require
    [clojure.tools.logging :as log]
-   [puppetlabs.bodgery.jruby :as jruby]
    [puppetlabs.services.file-serving.file-serving-core :as core]
    [puppetlabs.trapperkeeper.core :refer [defservice]]
    [puppetlabs.trapperkeeper.services :as services]
@@ -17,16 +16,20 @@
    [:JRubyPuppetService]]
   (init [this context]
     (log/info "Initializing FileServing service")
-    context)
+    (assoc context
+           :environments (atom {})
+           :ruby-mounts (atom [])
+           :jruby-service (services/get-service this :JRubyPuppetService)))
   (start [this context]
     (log/info "Starting FileServing service")
-    (log/info (with-out-str
-                (clojure.pprint/pprint
-                  (jruby/run-script! this "file_serving_shims/get_environments.rb"))))
+    (core/refresh-jruby-state! context)
 
     (log/info (with-out-str
-                (clojure.pprint/pprint
-                  (jruby/run-script! this "file_serving_shims/get_mounts.rb"))))
+                (clojure.pprint/pprint @(:environments context))))
+
+    (log/info (with-out-str
+                (clojure.pprint/pprint @(:ruby-mounts context))))
+
     context)
   (stop [this context]
     (log/info "Shutting down FileServing service")
